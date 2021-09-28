3e7c90984a96594452785779eb8d88a65d4b25c7
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-09-28 07:32:24 -0400

    adicionando sqlite3

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,6 @@
+import "reflect-metadata"; 
 import express from "express";
-import { request } from "http";
 
 const app = express();
-
-/*
-GET => buscar uma informaçao (listagem de contatos)
-POST => inserir uma informaçao dentro da API (criar usuario)
-PUT => alterar uma informaçao (alterar dados do usuario email, senha)
-DELETE => remover um dado produto ou usuario
-PATCH => alterar uma informacao especifica
-*/
-
-app.get("/test", (request, response) =>{
-  //response = entrando
-  //request = saindo
-  return response.send("metodo get")
-});
-
-app.post("/test-post", (request, response) =>{
-  return response.send("metodo post")
-});
 
 app.listen(8080, () => console.log("server is running: http://localhost:8080"));
