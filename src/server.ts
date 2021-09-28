a6f76c0c24efef161b9289465aa9fc97be2452d7
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-09-28 13:32:34 -0400

    criando entidade user  e colunas

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata"; 
 import express from "express";
 
+import "./database";
+
 const app = express();
 
 app.listen(8080, () => console.log("server is running: http://localhost:8080"));
