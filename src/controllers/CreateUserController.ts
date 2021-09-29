fb89a70f5393078506fa4a9006c6534dab9ad453
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-09-29 09:23:56 -0400

    aplicacao rodando

---
+++ src/controllers/CreateUserController.ts
@@ -0,0 +1,16 @@
+import { request, Request, Response} from "express"
+import { CreateUserService } from "../services/CreateUserService";
+
+class CreateUserController{
+  async handle(resquest: Request, response: Response){
+    const { name, email, admin } = request.body;
+
+    const createUserService = new CreateUserService();
+
+    const user = await createUserService.execute({name, email, admin});
+
+    return response.json(user);
+  }
+}
+
+export { CreateUserController}
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import "reflect-metadata"; 
 import express from "express";
 
+import { router } from "./routes";
+
 import "./database";
 
 const app = express();
+
+app.use(router);
 
 app.listen(8080, () => console.log("server is running: http://localhost:8080"));
