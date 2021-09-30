06522bd7357cf0f339d73496a4ca5875bcaff114
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-09-30 17:04:50 -0400

    Compliments recebidos e enviados, token

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,8 @@ import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticsteUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
 
 const router = Router();
 
@@ -12,6 +14,8 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
 
 router.post(
 "/tags", 
@@ -26,5 +30,18 @@ router.post(
 "/compliments", 
 ensureAuthenticated, 
 createComplimentController.handle);
+
+//usando o get pra listar
+router.get(
+  "/users/compliments/send",
+  ensureAuthenticated,
+  listUserSendComplimentsController.handle
+);
+router.get(
+  "/users/compliments/receive",
+  ensureAuthenticated,
+  listUserReceiveComplimentsController.handle
+);
+
 
 export { router }
--- src/services/ListUserSendComplimentsService.ts
+++ src/services/ListUserSendComplimentsService.ts
@@ -17,4 +17,4 @@ class ListUserSendComplimentsService {
   }
 }
 
-export { ListUserSendComplimentsService}
+export { ListUserSendComplimentsService }
